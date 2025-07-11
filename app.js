3720f36d40a4cf9e05012b79ae5b41692a4340c8
rtjil <RTJIL@users.noreply.github.com>
2025-07-11 13:54:54 +0000

    env fix 2

--- app.js
+++ app.js
@@ -2,30 +2,31 @@ import express from "express"
 import expressLayouts from "express-ejs-layouts"
 import { indexRouter } from "./routes/indexRouter.js"
 import path from "node:path"
-
 import dotenv from "dotenv"
+import { fileURLToPath } from 'url'
+
 dotenv.config()
 
 const app = express()
 
-export const __dirname = import.meta.dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-//serve static assets html,css etc
-const assetsPath = path.join(__dirname, "public")
+console.log("💡 views folder from env:", process.env.VIEWS_FOLDER)
+
+const assetsPath = path.join(__dirname, process.env.VIEWS_FOLDER || "public")
 app.use(express.static(assetsPath))
 
-//get info to body from form
 app.use(express.urlencoded({ extended: true }))
 
-//set ejs and use layout
-app.set("views", [path.join(__dirname, "views")])
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "layout")
 
 app.use("/", indexRouter)
 
-const PORT = process.env.PGPORT || 5432
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log("Server started on port: ", PORT)
+  console.log("🚀 Server started on port:", PORT)
 })
